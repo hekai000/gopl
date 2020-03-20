d0ef6bdc3bb411783d54a5f2fd5028229f6fcec6
hekai000 <675974119@qq.com>
2020-03-20 17:23:57 +0800

    补上chapter1

---
+++ src/gopl/chapter1/echo2/echo2.go
@@ -0,0 +1,19 @@
+// Echo2 prints its command-line arguments.
+package echo2
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	s, sep := "", " "
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+	}
+	fmt.Println(s)
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+
+}
